ad58032d0e657773b804f8d99f7bf98ceda587d4
tsubasa1122 <nakanot@open8.com>
2019-09-14 21:19:25 +0900

    jobの作成

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,11 +4,27 @@ class JobsController < ApplicationController
   end
 
   def new
+    @job = Job.new
   end
 
   def create
+    @job = Job.new
+    if @job.save
+      flash[:notice] = "求人がほぞんされました"
+      redirect_to @job
+    else
+      flash[:alert] = "求人の作成に失敗しました"
+      render :new
+    end
   end
 
   def show
+    @job = Job.find(params[:id])
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
