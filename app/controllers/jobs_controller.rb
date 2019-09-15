3a246173ad97aa90b5b6af83c0e6c84eb77a5654
tsubasa1122 <nakanot@open8.com>
2019-09-15 22:42:08 +0900

    一旦画像生成機能作成

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,13 +8,27 @@ class JobsController < ApplicationController
   end
 
   def create
-    @job = Job.new
+    @job = Job.new(job_params)
+    image = Magick::ImageList.new('./public/base_image.png')
+    draw = Magick::Draw.new
+    title = cut_text(@job.title)
+    draw.annotate(image, 0, 0, 0, -120, title) do
+      self.font = 'app/assets/NotoSansJP-Bold.otf'   #別のフォントをダウンロードした方はNotoSansJP-Bold.otfを変更
+      self.fill = '#000'
+      self.gravity = Magick::CenterGravity
+      self.font_weight = Magick::BoldWeight
+      self.stroke = 'transparent'
+      self.pointsize = 48
+    end
+    image_path = image.write(uniq_file_name).filename
+    image_url = cut_path(image_path)
+    @job.image_url = image_url
+
     if @job.save
       flash[:notice] = "求人がほぞんされました"
       redirect_to @job
     else
       flash[:alert] = "求人の作成に失敗しました"
-      render :new
     end
   end
 
@@ -26,5 +40,17 @@ class JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(:title, :content).merge(user_id: current_user.id)
+  end
+
+  def cut_path(url)
+    url.sub(/\.\/public\//, "")
+  end
+
+  def uniq_file_name
+    "./public/#{SecureRandom.hex}.png"
+  end
+
+  def cut_text(text)
+      text.scan(/.{1,15}/).join("\n")
   end
 end
