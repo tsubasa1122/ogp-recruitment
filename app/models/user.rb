1df2de5961c8e13678a444660376b2ca664c29c4
tsubasa1122 <nakanot@open8.com>
2019-09-16 19:53:35 +0900

    entryモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :jobs, dependent: :destroy
+  has_many :entries, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_120906) do
+ActiveRecord::Schema.define(version: 2019_09_16_104922) do
+
+  create_table "entries", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "job_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_entries_on_job_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
